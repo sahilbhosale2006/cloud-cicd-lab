cfc1628edc6cf3dda7cec74dd2584792f619d9fb
Sahil Bhosale <sahilbhosale2006@gmail.com>
2026-09-25 16:09:23 +0530

    Initial CI/CD lab application

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Cloud CI/CD Lab"
+
+@app.route("/student")
+def student():
+    return {
+        "name": "Student",
+        "course": "Cloud Computing and DevOps"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
